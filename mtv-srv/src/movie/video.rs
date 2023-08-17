49f952952a47f40188e7669c7097003b402baf0a
unknown <yes@noxue.com>
2023-08-17 23:47:11 +0800

    x

--- mtv-srv/src/movie/video.rs
+++ mtv-srv/src/movie/video.rs
@@ -1,11 +1,13 @@
+use chrono::Local;
 use mtv_dao::{
     movie::{
-        video::{Video, VideoList, UpdateVideo},
+        video::{UpdateVideo, Video, VideoList},
         Movie, UpdateMovie,
     },
     Db, Page,
 };
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 use crate::Result;
 
@@ -28,7 +30,24 @@ pub async fn list(movie_id: i32) -> Result<Vec<VideoList>> {
     Ok(v)
 }
 
-pub async fn get(video_id: i32, user_id: i32, is_admin: bool) -> Result<Video> {
+#[derive(Debug, Clone, Serialize)]
+pub struct VideoWithLike {
+    pub id: i32,
+    pub movie_id: i32,
+    pub name: String,
+    pub video: String,
+    pub price: i32,
+    pub status: i32,
+    pub likes: i32,
+    pub share: i32,
+    pub view: i32,
+    pub rank: i32,
+    pub is_liked: bool,
+    pub is_followed: bool,
+    pub create_time: chrono::DateTime<Local>,
+    pub update_time: chrono::DateTime<Local>,
+}
+pub async fn get(video_id: i32, user_id: i32, is_admin: bool) -> Result<VideoWithLike> {
     let conn = Db::get_conn();
     let v = mtv_dao::movie::video::get(&conn, video_id).await?;
 
@@ -37,14 +56,37 @@ pub async fn get(video_id: i32, user_id: i32, is_admin: bool) -> Result<Video> {
     }
     let v = v.unwrap();
 
+    // 是否点赞
+    let is_liked = mtv_dao::movie::video::is_liked(&conn, user_id, video_id).await?;
+    // 是否追剧
+    let is_followed = mtv_dao::movie::video::is_followed(&conn, user_id, v.movie_id).await?;
+
+    let v = VideoWithLike {
+        id: v.id,
+        movie_id: v.movie_id,
+        name: v.name,
+        video: v.video,
+        price: v.price,
+        status: v.status,
+        likes: v.likes,
+        share: v.share,
+        view: v.view,
+        rank: v.rank,
+        is_liked,
+        is_followed,
+        create_time: v.create_time,
+        update_time: v.update_time,
+    };
+
     // 不是管理员，就需要扣除金币，增加播放量
     if !is_admin {
         mtv_dao::movie::add_view(&conn, v.movie_id, v.id).await?;
         mtv_dao::movie::video::add_view_record(&conn, user_id, v.movie_id, video_id).await?;
         if v.price > 0 {
-
             // 已经付费过，直接返回
-            if mtv_dao::order::consume_record_by_user_id_and_video_id(&conn, user_id, video_id).await?{
+            if mtv_dao::order::consume_record_by_user_id_and_video_id(&conn, user_id, video_id)
+                .await?
+            {
                 return Ok(v);
             }
 
@@ -76,7 +118,6 @@ pub async fn get(video_id: i32, user_id: i32, is_admin: bool) -> Result<Video> {
     Ok(v)
 }
 
-
 pub async fn update(video_id: i32, update_video: &UpdateVideo) -> Result<Video> {
     let conn = Db::get_conn();
     let v = mtv_dao::movie::video::update(&conn, video_id, update_video).await?;
@@ -89,7 +130,3 @@ pub async fn delete(video_id: i32) -> Result<()> {
     mtv_dao::movie::video::delete(&conn, video_id).await?;
     Ok(())
 }
-
-
-
-
